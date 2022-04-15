31ee1182f8f149d74d85b115350445e755c18a22
kavchan <46276998+kavchan@users.noreply.github.com>
2022-04-14 20:00:31 -0500

    Working Create User PHP File
    Chris performed the edits to the createUser PHP interface and confirms it works.

--- createUser.php
+++ createUser.php
@@ -2,31 +2,35 @@
 // Change these two the local database settings
 $servername = "localhost";
 $username = "root";
-$password = "password";
-$dbname = "myDB";
+$password = "";
+$dbname = "car_rental_service";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check Connection
 if ($conn->connect_error) {
-    die("Connection Failed: ", $conn->connect_error);
+    die("Connection Failed: ");//, $conn->connect_error);
 }
 
 $stmt = $conn->prepare("INSERT INTO `user` (ssn, f_name, l_name, phone_number, street, city, state, zip) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt -> bind_param("issssssi"$ssn, $fName, $lName, $phoneNumber, $street, $city, $state, $zip);
+$stmt -> bind_param("issssssi",$ssn, $fName, $lName, $phoneNumber, $street, $city, $state, $zip);
 
 // Change the variables to the element names on the front end
+if ($_GET["ssn"] && $_GET["firstname"] && $_GET["lastname"] && $_GET["phone"] && $_GET["street"] && $_GET["city"] && $_GET["state"] && $_GET["zip"]) {
+    $ssn = $_GET['ssn'];
+    $fName = $_GET['firstname'];
+    $lName = $_GET['lastname'];
+    $phoneNumber = $_GET['phone'];
+    $street = $_GET['street'];
+    $city = $_GET['city'];
+    $state = $_GET['state'];
+    $zip = $_GET['zip'];
+}
 
-$ssn = $_POST['CHANGE_ME'];
-$fName = $_POST['CHANGE_ME'];
-$lName = $_POST['CHANGE ME'];
-$phoneNumber = $_POST['CHANGE_ME'];
-$street = $_POST['CHANGE_ME'];
-$city = $_POST['CHANGE_ME'];
-$state = $_POST['CHANGE_ME'];
-$zip = $_POST['CHANGE_ME'];
 
-stmt->execute();
+$stmt->execute();
+
+echo "New records created successfully";
 
 $stmt->close();
 $conn->close();

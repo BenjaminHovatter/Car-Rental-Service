69c9f2755829a1a463ddeba35009d73895871c21
Animeology <56809527+Animeology@users.noreply.github.com>
2022-04-14 22:01:16 -0500

    Update Car Rental Table Sets.sql

--- Car Rental Table Sets.sql
+++ Car Rental Table Sets.sql
@@ -112,9 +112,9 @@ CREATE TABLE payment
     FOREIGN KEY(card_holder) REFERENCES customer(c_id),
     
     CONSTRAINT PK_Payment UNIQUE (transaction_id),
-    CONSTRAINT Check_Exp_Date CHECK (SELECT *
+    CONSTRAINT Check_Exp_Date CHECK (SELECT exp_date
 									FROM payment
-									WHERE exp_date > NOW();)
+									WHERE exp_date > NOW())
 );
 
 -- Test Data for Payments
